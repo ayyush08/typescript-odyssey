81300260d28e68ee68e44f38bf22676a73005400
AYUSH KUMAR GUPTA <ayushkumargupta2908@gmail.com>
2024-11-16 17:03:55 +0530

    void and never

--- 03. Functions/functions.ts
+++ 03. Functions/functions.ts
@@ -19,10 +19,41 @@ let loginUser = (name:string,email:string,isPaid:boolean=false)=>{
     console.log(name,email,isPaid);
 }
 
+
+const getHello = (s:string):string => {
+    return `Hello ${s}`;
+}
+
+const heroes = ['Superman','Batman','Flash'];
+
+heroes.map(hero=>{ //ts automatically detects the  type is string we dont have to explicitly annotate it
+    return `${hero} is cool`;
+})
+heroes.map((hero):string=>{ // here we are explicitly annotating the type so that ts can check if the return type is string or not, good practice 
+    return `${hero} is cool`;
+})
+
+
+
+
+function consoleError(error: string): void{
+    console.error(error);
+    // return 1; // Error: Type '1' is not assignable to type 'void'.
+}
+
+function handleError(error: string): never{
+    throw new Error(error);
+    // return 1; // Error: Type '1' is not assignable to type 'never'.
+}
+    // void : return nothing() , never: never return anything(helpful in case of error handling)
+
+
+
 console.log(getUpperCasedString('hello')); // HELLO
 console.log(addTwo(2)); // 4
 signUpUser('Ayush','ayush@gmail.com',true); 
 loginUser('Ayush','ayush@gmail.com');
+console.log(getHello('Ayush')); // Hello Ayush
 
 
 export {} //to avoid Scope error
