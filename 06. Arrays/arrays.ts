e0f1f7211d1833fd754ddcd27b6634bc74089d1a
AYUSH KUMAR GUPTA <ayushkumargupta2908@gmail.com>
2024-11-22 18:45:16 +0530

    Arrayd basics

---
+++ 06. Arrays/arrays.ts
@@ -0,0 +1,22 @@
+const superHeroes: string[] = []
+// const heroPower: number[] = [] this OR the one below
+
+const heroPower: Array<number> = []
+
+type User = {
+    name: string,
+    isActive: boolean
+}
+
+const allUsers: User[] = []
+
+const MLModels: number[][] = [
+    [255, 255, 255],
+]
+
+
+
+
+superHeroes.push("Spiderman")
+heroPower.push(100)
+allUsers.push({name: "Peter Parker", isActive: true})
