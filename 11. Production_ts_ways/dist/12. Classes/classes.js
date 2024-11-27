28a79bdfed95bd501d2b189025f1bb28b609bc2e
AYUSH KUMAR GUPTA <ayushkumargupta2908@gmail.com>
2024-11-27 17:14:22 +0530

    Private and Public

--- 11. Production_ts_ways/dist/12. Classes/classes.js
+++ 11. Production_ts_ways/dist/12. Classes/classes.js
@@ -2,9 +2,20 @@
 class User {
     constructor(email, name) {
         this.city = "Azamgarh";
-        this.email = email;
+        this.email = email; // not marked  is considereeed public by default 
         this.name = name;
+        this.city = 'delhi';
     }
 }
 const ayush = new User('adandakn@fma,com', 'ayush');
 // ayush.city = 'delhi'; // not allowed as it is readonly
+class User2 {
+    constructor(email, name) {
+        this.email = email;
+        this.name = name;
+        this.city = "Azamgarh";
+        this.email = email; // not marked  is considereeed public by default 
+        this.name = name;
+        this.city = 'delhi';
+    }
+}
--- 11. Production_ts_ways/src/12. Classes/classes.ts
+++ 11. Production_ts_ways/src/12. Classes/classes.ts
@@ -1,12 +1,22 @@
 class User{
-    email: string;
+    public email: string;
     name: string;
-    readonly city: string="Azamgarh";
+    private readonly city: string="Azamgarh";
     constructor(email:string,name:string){
-        this.email = email;
+        this.email = email; // not marked  is considereeed public by default 
         this.name = name;
+        this.city = 'delhi'; 
     }
 }
 
 const ayush = new User('adandakn@fma,com','ayush');
 // ayush.city = 'delhi'; // not allowed as it is readonly
+
+class User2{
+    private readonly city: string="Azamgarh";
+    constructor(public email:string,public name:string){
+        this.email = email; // not marked  is considereeed public by default 
+        this.name = name;
+        this.city = 'delhi'; 
+    }
+}
