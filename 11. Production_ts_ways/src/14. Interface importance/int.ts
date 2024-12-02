329c8e0a395b6ef01e205d9692d76da20b754e10
AYUSH KUMAR GUPTA <ayushkumargupta2908@gmail.com>
2024-12-02 10:04:31 +0530

    Importance of Interfaces

---
+++ 11. Production_ts_ways/src/14. Interface importance/int.ts
@@ -0,0 +1,37 @@
+interface TakePhoto{
+    cameraMode: string;
+    filter: string;
+    burst: number;
+}
+
+interface Story{
+    createStory(): void;
+
+}
+
+
+
+//Use Case - implementing protocols for a app feature
+ //interface never involves definition, it always inclines the must have things in the class.
+class Instagram implements TakePhoto{
+    constructor(
+        public cameraMode: string,
+        public filter: string,
+        public burst: number
+    ){}
+}
+
+// more than necessary is allowed but less than necessary is not allowed
+
+class Youtube implements TakePhoto,Story{
+    constructor(
+        public cameraMode: string,
+        public filter: string,
+        public burst: number,
+        public short: string
+    ){}
+    createStory(): void {
+        console.log('Story created');
+        
+    }
+}
